0dc81dffa9a847a30183d53f02022e2a0bea8ed1
Kevin TSI <kevtsiii@gmail.com>
2021-03-04 10:36:40 +0100

    Update notebook

--- run_docker_elastic.sh
+++ run_docker_elastic.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo docker run -d --name elasticsearch-container -v $(pwd)/elas1:/usr/share/elasticsearch/data -p 9200:9200 -p 9300:9300 -e "ELASTICSEARCH_HEAP_SIZE=128m" docker.elastic.co/elasticsearch/elasticsearch:7.11.1
+sudo docker run -d --name elasticsearch-container -v $(pwd)/elas1:/usr/share/elasticsearch/data -p 9200:9200 -p 9300:9300 -e "xpack.security.enabled=false" -e "discovery.type=single-node" docker.elastic.co/elasticsearch/elasticsearch:7.11.1
 
 printf "✅ Elastic Container OK ✅ \n"
 
